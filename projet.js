f6ccf842388f2af711a04004cdd280fd7dfd127f
Nils <72611176+Nils92@users.noreply.github.com>
2021-05-17 16:53:47 +0200

    Add files via upload

---
+++ projet.js
@@ -0,0 +1,17 @@
+
+
+
+function Pico() {
+   document.getElementById("projet").innerHTML = "<h1>Pico</h1><img src=\"img/pico2.gif\" id=\"gifpico\" name=\"imgCode\"><img src=\"img/pico.gif\" class=\"gifun\" name=\"imgCode\"><div class=\"buttone\"><a href=\"pico.html\">Jouer au jeu&nbsp;</a><div class=\"mask\"></div></div><div class=\"buttone\"><a href=\"https://github.com/Nils92/France.P8/blob/main/france.p8\">Voir le code&nbsp;</a><div class=\"mask\"></div></div><div id=\"texte\"><p>Language : </p><div class=\"tags\"><div>Lua</div></div><p><br>Groupe : 2 personnes<br><br>Jeux de foot \"1 contre 1\" réalisé via la console virtuelle pico 8.<br>Voyagez entre Punta Cana et Paris dans ce jeux qui risque de faire de l'ombre a Fifa21</p></div>";
+};
+
+function Chatbot() {
+   document.getElementById("projet").innerHTML = "<h1>Chatbot</h1><img src=\"img/Chatbot2.gif\" class=\"gifdeux\" name=\"imgCode\"><img src=\"img/chatbot.gif\" class=\"gifun\" name=\"imgCode\"><div class=\"buttone\"><a href=\"https://github.com/adatechschool/pc-csml-love-is-here\">Voir le code&nbsp;</a><div class=\"mask\"></div></div><div id=\"texte\"><p>Language : </p><div class=\"tags\"><div>Csml</div></div><p><br>Groupe : 4 personnes<br><br>Le chatbot permet de faire des blagues, afficher des gifs à partir d'un mot clef, de jouer au pendu et de donner la météo dans la ville voulue.</p></div>";
+};
+
+function Swift() {
+   document.getElementById("projet").innerHTML = "<h1>Swift</h1><img src=\"img/swift.gif\" class=\"gifdeux\" name=\"imgCode\"><img src=\"img/swift2.gif\" id =\"gifswift\" name=\"imgCode\"><div class=\"buttone\"><a href=\"https://github.com/margauxln/wavySpots\">Voir le code&nbsp;</a><div class=\"mask\"></div></div><div id=\"texte\"><p>Language : </p><div class=\"tags\"><div>Swift</div></div><p><br>Groupe : 4 personnes<br><br>Découvrez l'application la plus pratique pour les Parisiens voulant faire du surf.<br> Réalisée avec Xcode et Airtable.</p></div>";
+};
+function Php() {
+   document.getElementById("projet").innerHTML = "<h3>Réseau social</h3><img src=\"img/php2.png\" class=\"gifdeux\" name=\"imgCode\"><img src=\"img/php.png\" class=\"gifun\" name=\"imgCode\"><div class=\"buttone\"><a href=\"https://github.com/Marine139/socialnetwork\">Voir le code&nbsp;</a><div class=\"mask\"></div></div><div id=\"texte\"><p>Language : </p><div class=\"tags\"><div>Php</div></div><p><br>Groupe : 4 personnes<br><br>Le réseau permet l’affichage des messages (posts) postés par les utilisateurs (users), l’inscription des utilisateurs(users), leur mise en relation (followers), et l’ajout de mentions “J’aime”(likes).</p></div>";
+};
